d470d13b9b88e601f81d7b615ee7cf8397262759
hwangs12 <sungjun94@gmail.com>
2025-04-26 00:53:57 -0400

    hoho

--- understanding_and_using_c_pointers/01.01.01_intro.c
+++ understanding_and_using_c_pointers/01.01.01_intro.c
@@ -1,3 +1,7 @@
 // Pointers are complex
 // Understanding requires a working knowledge of the program stack and heap
 
+// Approach
+// if we allocate a struct whose logical size is 45, actually more than 45 bytes are typically allocated
+// memory allocated may be fragmented
+
