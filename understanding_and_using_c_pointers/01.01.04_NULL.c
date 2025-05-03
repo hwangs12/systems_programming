f301be3239a259ac850e2924cae45a9a0e24aaec
hwangs12 <sungjun94@gmail.com>
2025-05-03 04:28:39 -0400

    fun

--- understanding_and_using_c_pointers/01.01.04_NULL.c
+++ understanding_and_using_c_pointers/01.01.04_NULL.c
@@ -2,3 +2,13 @@
 // different.
 // pointer containing NULL does not reference
 // any location in memory
+
+// NULL should not be used in contexts other than 
+// pointers
+
+// Meaning of zero changes. example
+int num;
+int *pi = 0; // zero refers to the null pointer
+pi = &num;
+*pi = 0; // zero refers to the int zero
+
