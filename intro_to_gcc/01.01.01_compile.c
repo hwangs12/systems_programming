8b1fc566f7499b3c51d80e4cd10fe606f3be4f13
hwangs12 <sungjun94@gmail.com>
2025-05-03 13:59:15 -0400

    fun

---
+++ intro_to_gcc/01.01.01_compile.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Hello, world!\n");
+    return 0;
+}
+
+// we can compile this with
+// gcc -Wall name_of_file -o output_file
+// this compiles the source code
