00de0ea6c4cb8556ad20816fb49ff90080213345
hwangs12 <sungjun94@gmail.com>
2025-05-23 11:39:46 -0400

    fun

--- c_programming_modern_approach/02.01.02_exercise.c
+++ c_programming_modern_approach/02.01.02_exercise.c
@@ -2,12 +2,16 @@
 
 int main(void)
 {
-    printf("Parkinson's Law:\nWork expands so as to ");  // statements
-    printf("fill the time\n"); // statements
-    printf("available for its completion.\n"); // statements
-    return 0; // statements
+    printf("Parkinson's Law:\nWork expands so as to "); // statements
+    printf("fill the time\n");                          // statements
+    printf("available for its completion.\n");          // statements
+    return 0;                                           // statements
 }
 
 /**
  * Identify the directives and satements (above)
+ * What output does the program produce?
+ * Parkinson's Law:
+ * Work expands so as to fill the time
+ * available for its completion.
  */
