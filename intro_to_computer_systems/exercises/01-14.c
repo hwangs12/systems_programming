197d74a11975ffcf3469ee38eb416e03cfe9615c
hwangs12 <sungjun94@gmail.com>
2025-06-13 22:24:22 -0400

    fun

---
+++ intro_to_computer_systems/exercises/01-14.c
@@ -0,0 +1,44 @@
+/**
+ * Suppose we wish to put a set of names
+ * in alphabetical order
+ * We call the act of doing so sorting
+ * One algorithm that can accomplish that
+ * is called the bubble sort.
+ * 
+ * We could then program our buble
+ * sort algorithm in C and compile the C program
+ * to execute on an x86 ISA.
+ * 
+ * The x86 ISA can be implemented with
+ * an Intel Pentium IV microarchitecture.
+ * 
+ * Let us call the sequence "Bubble Sort,
+ * C program, x86 ISA, Pentium IV 
+ * microarchitecture" one transformation
+ * process
+ * 
+ * Assume we have available four sorting 
+ * algorithms and can program in C, C++,
+ * Pascal, Fortra, and COBOL. 
+ * We have available compilers that can
+ * translate from each of these
+ * to either x86 or SPARC, and we
+ * have available three different
+ * microarchitectures for x86 and three
+ * different microarchitectures for
+ * SPARC.
+ * 
+ * How many transformation process are possible?
+ * A. 120
+ * 
+ * Write three examples of transformation processes.
+ * A. BUBBLE SORT, C, X86 ISA, PENTIUM IV
+ * A. BUBBLE SORT, C++, SPARC ISA, Ultra machines
+ * A. MERGE SORT, COBOL, X86 ISA, 
+ * 
+ * How many transformation processes are possible
+ * if instead of three different microarchitectures
+ * for x86 and three different microarchitectures for
+ * SPARC, there were two for x86 and four for SPARC?
+ * A. SAME
+ */
