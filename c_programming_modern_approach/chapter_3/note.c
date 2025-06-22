9980afad9b76e969d390eca34894e925de2d0d6f
hwangs12 <sungjun94@gmail.com>
2025-06-21 22:42:34 -0400

    fun

--- c_programming_modern_approach/chapter_3/note.c
+++ c_programming_modern_approach/chapter_3/note.c
@@ -3,12 +3,23 @@
 int main(void)
 {
     printf("%f %d\n", 3, 3.14);
-
+    printf("%d %1.4f\n", 3, 3.14);
+    printf("%d %f\n", 3, 3.14);
+    
     // using e
     double value = 12345.678;
     printf("Value in scientific notation: %e\n", value);
     printf("Value in scientific notation: %2.3e\n", value);
     printf("Value in scientific notation: %.3e\n", value);
     printf("Value in scientific notation: %1.-1e\n", value);
+    
+    // using g specifier
+    float val = 12345.6783928f;
+    printf("Value in scientific notation: %g\n", val);
+    val = 1.2938f;
+    printf("Value in scientific notation: %g\n", val);
+    val = 1.29383927f;
+    printf("Value in scientific notation: %g\n", val);
+    
     return 0;
 }
