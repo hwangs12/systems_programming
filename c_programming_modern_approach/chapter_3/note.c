325c41c88ee2f8ab95943de3089d8d8b694aa85d
hwangs12 <sungjun94@gmail.com>
2025-06-21 22:28:11 -0400

    fun

--- c_programming_modern_approach/chapter_3/note.c
+++ c_programming_modern_approach/chapter_3/note.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     printf("%f %d\n", 3, 3.14);
+
+    // using e
+    double value = 12345.678;
+    printf("Value in scientific notation: %e\n", value);
+    printf("Value in scientific notation: %2.3e\n", value);
+    printf("Value in scientific notation: %.3e\n", value);
+    printf("Value in scientific notation: %1.-1e\n", value);
     return 0;
 }
