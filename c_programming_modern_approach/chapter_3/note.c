b3c9d1c431210ed9ff4baa8676837f6731e1dc19
hwangs12 <sungjun94@gmail.com>
2025-06-27 22:52:59 -0400

    fun

--- c_programming_modern_approach/chapter_3/note.c
+++ c_programming_modern_approach/chapter_3/note.c
@@ -35,7 +35,8 @@ int main(void)
     x = 839.21f;
 
     printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
-    printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
+    printf("|%10.3f|%10.3e|%-10g|\n", x, x, x); // here what you notice is that these specifiers are for floats only 
+    // let's stick to that. no doubles here. 
 
     return 0;
 }
