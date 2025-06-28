8c057da6272bbca842b66b1e88ed52a003bd54ef
hwangs12 <sungjun94@gmail.com>
2025-06-27 22:59:43 -0400

    fun

--- c_programming_modern_approach/chapter_3/note.c
+++ c_programming_modern_approach/chapter_3/note.c
@@ -25,8 +25,7 @@ int main(void)
     printf("Value in scientific notation: %g\n", val1);
     val = 127387181.29383927f;
     printf("Value in scientific notation: %g\n", val);
-    return 0;
-
+    // return 0;  if you put return  here below will not execute. thanks return
 
     // awesome example from the book and what is |?
     int i;
@@ -35,8 +34,11 @@ int main(void)
     i = 40;
     x = 839.21f;
 
-    printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
+    printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i); // here minus used to right pad (occupy five space and right pad the value). m is space occupied and p is num digit to display
     printf("|%10.3f|%10.3e|%-10g|\n", x, x, x); // here what you notice is that these specifiers are for floats only 
+    // above 10.3f -> 10 space right align 3 decimals trailing zero for free ones.
+    // 10.3e -> 10 space use e 3 decimal points, right aligned
+    // -10g -> 10 space no trailing zeros left align
     // let's stick to that. no doubles here. 
 
     return 0;
