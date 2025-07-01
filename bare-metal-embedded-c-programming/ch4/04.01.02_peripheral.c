e8e6ec9916de9a0d823d03d145f1bfd161f4e8aa
hwangs12 <sungjun94@gmail.com>
2025-06-30 23:19:27 -0400

    fun

---
+++ bare-metal-embedded-c-programming/ch4/04.01.02_peripheral.c
@@ -0,0 +1,8 @@
+/**
+ * PERIPHERAL MEMORY
+ * 
+ * dedicated to managing and interfacing with the microcontroller's onboard peripherals
+ * include timers, communication interfaces, and analog-to-digital converters
+ * 
+ * made up of registers that are used to configure and manage these peripherals
+ */
