e2f2ed66059130d759681eb0d45bafde8abfb99b
hwangs12 <sungjun94@gmail.com>
2025-07-01 23:22:32 -0400

    fun

--- bare-metal-embedded-c-programming/ch4/04.01.02_peripheral.c
+++ bare-metal-embedded-c-programming/ch4/04.01.02_peripheral.c
@@ -5,4 +5,13 @@
  * include timers, communication interfaces, and analog-to-digital converters
  * 
  * made up of registers that are used to configure and manage these peripherals
+ * 
+ * MICROCONTROLLER ARCHITECTURE
+ * 
+ * MEMORY-MAPPED INPUT/OUPUT
+ * - TECHNIQUE WHERE PERIPHERAL REGISTERS ARE ASSIGNED SPECIFIC ADDRESSES IN THE SYSTEM'S MEMORY SPACE
+ * ALLOWS FIRMWARE TO INTERACT WITH HARDWARE PERIPHERALS BY READING FROM OR WRITING TO THESE MEMORY ADDRESSES,
+ * JUST AS IT WOULD WITH REGULAR MEMORY.
+ * 
+ * 
  */
