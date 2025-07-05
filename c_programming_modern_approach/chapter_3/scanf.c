5e357913013e7fff3321eb662353f493a07e7f19
hwangs12 <sungjun94@gmail.com>
2025-07-04 23:35:09 -0400

    fun

--- c_programming_modern_approach/chapter_3/scanf.c
+++ c_programming_modern_approach/chapter_3/scanf.c
@@ -8,5 +8,13 @@ int main(void) {
 
     scanf("%d%d%f%f", &i, &j, &x, &y);
     printf("%d   %d     %f   %f", i, j, x, y);
+
+    // scanf can actually do much more than indicated
+
+    // it is essentially a pattern matching that tries to match up groups of input chars with conversion specifications
+
+    // like the printf, scanf is controlled by the format string
+
+    // when it is called, scanf begins processing the information inteh string, starting at the left. 
     return 0;
 }
