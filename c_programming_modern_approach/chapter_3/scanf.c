d1572b49f584eaea685ffaea240820cca10ec49e
hwangs12 <sungjun94@gmail.com>
2025-07-05 23:37:30 -0400

    update

--- c_programming_modern_approach/chapter_3/scanf.c
+++ c_programming_modern_approach/chapter_3/scanf.c
@@ -15,6 +15,20 @@ int main(void) {
 
     // like the printf, scanf is controlled by the format string
 
-    // when it is called, scanf begins processing the information inteh string, starting at the left. 
+    // when it is called, scanf begins processing the information inteh string, starting at the left.
+    
+    // for each conversion specification in the format string, scanf tries to locate an item of the appropraite type in the input data
+
+    // skipping blank space if necessary.
+
+    // scanf then reads the item, stopping when it encounters character that can't possibly belong to the item. 
+
+    // if the item was read successfully scanf continues processing the rest of the format string
+
+    // if any item is not read successfully, scanf returns immeidately without looking at the rest of the format string
+
+    // as it searches for the beginning of a number, scanf ignores white-space chars (space, horizontal and vertical tab, form-feed, and new -line characters).
+
+    
     return 0;
 }
