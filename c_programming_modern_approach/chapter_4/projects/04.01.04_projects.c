0f658b40949fa14899ffb0a6ebb63c0705d58555
hwangs12 <sungjun94@gmail.com>
2025-07-24 23:08:52 -0400

    haha

---
+++ c_programming_modern_approach/chapter_4/projects/04.01.04_projects.c
@@ -0,0 +1,29 @@
+/**
+ * Write a program that reads an integer
+ * entered by the user and displays it
+ * in octal 
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    int remainder;
+    int octal = 0;
+    printf("Enter a number between 0 and 32767: ");
+    scanf("%d", &num);
+    
+    int firstDigit = num / 4096;
+    num %= 4096;
+    int secondDigit = num / 512;
+    num %= 512;
+    int thirdDigit = num / 64;
+    num %= 64;
+    int fourthDigit = num / 8;
+    num %= 8;
+    int fifthDigit = num;
+
+
+    printf("In octal, your number is: %d%d%d%d%d", firstDigit, secondDigit, thirdDigit, fourthDigit, fifthDigit);
+}
