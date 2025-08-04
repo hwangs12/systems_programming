797c118a9fd08bbc18cf34a177f34049d31c840d
hwangs12 <sungjun94@gmail.com>
2025-08-03 23:46:33 -0400

    have fun

--- c_programming_modern_approach/chapter_5/exercises/05.01.01_exercise.c
+++ c_programming_modern_approach/chapter_5/exercises/05.01.01_exercise.c
@@ -11,7 +11,8 @@ int main(void)
     int i, j, k;
     i = 2;
     j = 3;
-    k = i * j == 6; // order of precedence? relational has lower than arithmetic
+    k = i * j == 6; // order of precedence? relational has lower than arithmetic so this
+    // this is equal to ((i*j)==6)
     if (k == 1) {
         printf("k is 1 as expected");
     }
