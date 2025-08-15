b6fb571727863e6ece092ef16a68408a47a41f28
hwangs12 <sungjun94@gmail.com>
2025-08-14 23:33:19 -0400

    fun

---
+++ c_programming_modern_approach/chapter_5/exercises/05.01.04_exercise.c
@@ -0,0 +1,22 @@
+/**
+ * Write a single expression
+ * whose value is either -1, 0, or +1
+ * depending on whether i is less than
+ * equal to or greater than j
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+    (i > j) - (i < j);
+    // how did someone find
+    // this solution
+
+
+
+
+
+    return 0;
+}
