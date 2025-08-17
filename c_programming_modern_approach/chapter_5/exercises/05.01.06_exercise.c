10282082af2ed5d4e3582573233526c7eb0436c8
hwangs12 <sungjun94@gmail.com>
2025-08-16 21:51:28 -0400

    fun

--- c_programming_modern_approach/chapter_5/exercises/05.01.06_exercise.c
+++ c_programming_modern_approach/chapter_5/exercises/05.01.06_exercise.c
@@ -1,3 +1,22 @@
 /**
  * Is the following if statement legal?
+ * If so, what does it do when n is equal to 5?
+ * 
  */
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 5;
+    if (n == 1 - 10) {
+        printf("n is between 1 and 10");
+    }
+
+    // if n is 5
+    // arithmetic operation is higher than
+    // equality
+    // so, 1 - 10 is -9
+    // and n == -9 is false so
+    // there's not print statement
+
+}
